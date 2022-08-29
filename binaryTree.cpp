d405c120475f9031aed537a8636c6b1603d5a34e
dhruvchodha7 <dhruvchodha@live.com>
2022-08-29 17:45:59 +0530

    added level order traversal

---
+++ binaryTree.cpp
@@ -0,0 +1,77 @@
+// tree is a non linear data structure
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node *buildTree(node *root)
+{
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+    if (data == -1)
+        return NULL;
+    cout << "For left side " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "For right side " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+
+void levelOrderTraversals(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+
+        if (temp = NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+                q.push(NULL);
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+                q.push(temp->left);
+
+            if (temp->right)
+                q.push(temp->right);
+        }
+    }
+}
+
+int main()
+{
+    node *root = NULL;
+
+    // creating a tree
+    root = buildTree(root);
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    // level order
+    cout << "Printing level order traversal ";
+    levelOrderTraversals(root);
+    return 0;
+}
--- implementation.cpp
+++
@@ -1,9 +0,0 @@
-//tree is a non linear data structure
-
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    
-    return 0;
-}
